f82cdf38c95daed283e1b6ad8f7b1b58b67ab1ac
Afonso Martins <up202005900@edu.fe.up.pt>
2025-03-07 23:42:33 +0000

    PrimeNG config

---
+++ holdingpoint/src/app/firebase-config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCdLZNAz6nTf3QPVKGpD3Igy5GBhr9iC_4",
+    authDomain: "holdingpoint-56866.firebaseapp.com",
+    projectId: "holdingpoint-56866",
+    storageBucket: "holdingpoint-56866.firebasestorage.app",
+    messagingSenderId: "826929751544",
+    appId: "1:826929751544:web:99b0d6463f38579d97a20d",
+    measurementId: "G-PJ6W949NM4"
+};
