2b2ab810ae778b0981d202afc2a8e050d748f0c1
Afonso Martins <up202005900@edu.fe.up.pt>
2025-03-08 14:48:46 +0000

    Cleanup

--- holdingpoint/src/app/app.module.ts
+++ holdingpoint/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { GeneralModule } from './modules/general/general.module';
 import { AppRoutingModule } from './app-routing.nodule';
